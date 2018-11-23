bfda4fe13b96996d6d4d3235f8d87aee3423e8af
vitorb19 <vitorhugobarbosa13@gmail.com>
2018-11-23 23:20:18 +0000

    Updated version

--- src/Utilizador.h
+++ src/Utilizador.h
@@ -2,52 +2,83 @@
 #define UTILIZADOR_H_
 
 #include <string>
-/*#include <vector>*/
-
 #include "CartaoCredito.h"
 #include "Biblioteca.h"
 
+/**
+ * Utilizador que foi declarado no Sistema
+ */
 class Utilizador {
 
 private:
-	std::string nome;
-	std::string email;
-	size_t idade;
-	std::string morada;
-	std::vector<CartaoCredito> cc;
-	Biblioteca conjuntoTitulos;
+	std::string nome;/**< Nome do utilizador */
+	std::string email;/**< Email do utilizador */
+	size_t idade;/**< Idade do utilizador */
+	std::string morada;/**< Morada do utilizador */
+	std::vector<CartaoCredito> cc;/**< Vetor com os cartoes de credito do utilizador */
+	Biblioteca conjuntoTitulos;/**< Biblioteca com todos os Titulos do utilizador */
 
 public:
-	Utilizador(const std::string nome = "", const std::string email = "", /*construtor nao inclui cartoes de credito*/const size_t idade = 0, const std::string morada = "", const Biblioteca b =Biblioteca());
+	/**
+	 * @brief Construtor da classe Utilizador
+	 * @param nome - Nome atribuido ao utilizador
+	 * @param email - Email atribuido ao utilizador
+	 * @param idade - Idade atribuido ao utilizador
+	 * @param morada - Morada atribuido ao utilizador
+	 * @param b - Biblioteca atribuida ao utilizador
+	 */
+	Utilizador(const std::string nome = "", const std::string email = "", const size_t idade = 0, const std::string morada = "", const Biblioteca b=Biblioteca());
+
+	/**
+	 * @brief Permite obter o nome do utilizador 
+	 */
 	std::string getNome() const;
+	
+	/**
+	 * @brief Permite obter o email do utilizador 
+	 */
 	std::string getEmail() const;
+	
+	/**
+	 * @brief Permite obter a idade do utilizador 
+	 */
 	size_t getIdade() const;
+	
+	/**
+	 * @brief Permite obter a morada do utilizador
+	 */
 	std::string getMorada() const;
+
+	/**
+	 * @brief Permite obter o vetor de cartoes de credito do utilizador 
+	 */
 	std::vector<CartaoCredito> getCc() const;
+
+	/**
+	 * @brief Permite obter a biblioteca do utilizador 
+	 */
 	Biblioteca getBiblioteca() const;
+
+	/**
+	 * @brief Overload do operador de igualdade para a classe Utilizador 
+	 */
 	bool operator ==(const Utilizador & U);
-	/*
-	 * adiciona um cartao de credito caso este ainda nao exista
-	 * param C cartao de credito
-	 * return true se foi inserido senao falso
+	
+	/**
+	 * @brief Adiciona um cartao de credito caso este ainda nao exista
+	 * @param C - cartao de credito
 	 */
 	bool adicionaCartaoCredito(const CartaoCredito & C);
 
-	/*
-	 * ve se pode adicionar um titulo à biblioteca
-	 * @param T titulo a acrescentar
-	 * @param plataforma onde vamos comprar titulo
-	 * return true ou false mediante se foi ou nao possivel adicionar um titulo
-	 */
+	/**
+	 * @brief Adiciona um titulo ao utilizador 
+	 * @param T - Titulo a acrescentar
+	 * @param c - Cartao de credito para efutuar compra
+ 	 */
 	void AdicionaTitulo(Titulo * T, CartaoCredito & c);
 
-	/*
-	 * verifica a plataforma preferida do utilizador
-	 */
-	std::string PlataformaPreferida() const;
-
-	/*
-	 * retorna valor gasto pelo utilizador
+	/**
+	 * @brief Calcula o preco total da biblioteca do utilizador
 	 */
 	float getGastos() const;
 
